d9509ba05296478ce77211553a4ae7c624755c5d
TheMaikXX <filip.jakab@keenmate.com>
2017-10-22 13:17:56 +0200

    ESlint basic configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,12 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+		"quotes": ["warn", "double"],
+		"no-tabs": "off",
+		"indent": ["warn", "tab"],
+		"eqeqeq": "warn",
+		"no-new": "off",
+		"no-unused-vars": "warn"
   }
 }
